29dc730a2b267e944fbe7d419f4d3c7d7ae7bcb4
root <root@ellejaclyn1c.mylabserver.com>
2019-05-23 11:47:19 +0000

    Refactored module to support Debian-based hosts

---
+++ manifests/params.pp
@@ -0,0 +1,13 @@
+# @summary 
+#   Module variables
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
