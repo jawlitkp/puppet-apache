1ac1ccead6fba473c74a9372670e5206c2bcc71d
Jawlit Patel <jawlit.patel@gmail.com>
2021-02-09 19:11:11 -0500

    debug

--- lib/facter/node_classifier.rb
+++ lib/facter/node_classifier.rb
@@ -1,7 +1,16 @@
 require 'socket'
-require 'yaml'
-require File.join(File.dirname(__FILE__), 'func')
+#require File.join(File.dirname(__FILE__), 'func')
 #require File.join(File.dirname(__FILE__), 'node_definitions')
+
+def ip_addr_in_range?(low, high, addr)
+    int_addr = numeric_ip(addr)
+    int_addr <= numeric_ip(high) && int_addr >= numeric_ip(low)
+  end
+  
+  def numeric_ip(ip_str)
+    ip_str.split('.').inject(0) { |ip_num, part| ( ip_num << 8 ) + part.to_i }
+  end
+
 
 Facter.add(:classification_hash) do
     setcode do
@@ -19,7 +28,8 @@ Facter.add(:classification_hash) do
             ip = IPSocket.getaddress(Socket.gethostname)
             fqdn = Socket.gethostname
             hostname = fqdn.split(/[.\s]/)[0]
-
+            
+            classification_hash["ip"] = ip
 
             subnets.each do |key, value|
                     if ip_addr_in_range?(value["low"], value["high"], ip)
