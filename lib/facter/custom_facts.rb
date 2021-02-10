379832c6d5e07296a12f56c6afceb2ea756af5a0
Jawlit Patel <jawlit.patel@gmail.com>
2021-02-09 19:14:15 -0500

    debug

--- lib/facter/custom_facts.rb
+++ lib/facter/custom_facts.rb
@@ -1,15 +1,13 @@
 require 'socket'
-#require File.join(File.dirname(__FILE__), 'func')
-#require File.join(File.dirname(__FILE__), 'node_definitions')
 
 def ip_addr_in_range?(low, high, addr)
     int_addr = numeric_ip(addr)
     int_addr <= numeric_ip(high) && int_addr >= numeric_ip(low)
-  end
+end
   
-  def numeric_ip(ip_str)
+def numeric_ip(ip_str)
     ip_str.split('.').inject(0) { |ip_num, part| ( ip_num << 8 ) + part.to_i }
-  end
+end
 
 
 Facter.add(:classification_hash) do
--- lib/facter/hardware_platform.rb
+++
@@ -1,7 +0,0 @@
-# hardware_platform.rb
-
-Facter.add('hardware_platform') do
-    setcode do
-      Facter::Core::Execution.execute('/bin/uname --hardware-platform')
-    end
-  end
